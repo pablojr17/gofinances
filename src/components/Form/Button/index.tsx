f879563b83ad21c2cb213a0c4918a8cd76f7e222
pablojr17 <pablojr17@gmail.com>
2021-06-12 15:54:41 -0300

    Tipando o Button

--- src/components/Form/Button/index.tsx
+++ src/components/Form/Button/index.tsx
@@ -3,12 +3,13 @@ import { RectButtonProps } from 'react-native-gesture-handler';
 import { Container, Title } from './styles';
 
 interface Props extends RectButtonProps {
-  title: string
+  title: string;
+  onPress: () => void;
 }
 
-export function Button({ title, ...rest }: Props) {
+export function Button({ title, onPress, ...rest }: Props) {
   return (
-    <Container {...rest} >
+    <Container onPress={onPress} {...rest} >
       <Title>{title}</Title>
     </Container>
   )
