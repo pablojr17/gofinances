c1793d82ee48aff2308f70ceb45b42e7fa34e6b8
pablojr17 <pablojr17@gmail.com>
2021-06-11 22:23:39 -0300

    Tab Navigation

--- App.tsx
+++ App.tsx
@@ -8,12 +8,11 @@ import {
   Poppins_500Medium,
   Poppins_700Bold
 } from '@expo-google-fonts/poppins'
+import { NavigationContainer } from '@react-navigation/native';
 
 import theme from './src/global/styles/theme';
-import { Register } from './src/screens/Register';
 import { StatusBar } from 'react-native';
-import { Dashboard } from './src/screens/Dashboard';
-import { CategorySelect } from './src/screens/CategorySelect';
+import { AppRoutes } from './src/routes/app.routes';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -29,7 +28,10 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar backgroundColor={theme.colors.primary} />
-      <Register />
+      <NavigationContainer>
+        <AppRoutes />
+
+      </NavigationContainer>
     </ThemeProvider>
   )
 }
